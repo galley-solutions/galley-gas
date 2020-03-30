71a7d8fb34612130d8443c764c93967f9b192225
Benji Koltai <koltaibenji@gmail.com>
2020-03-30 13:55:51 -0700

    Working fetch of recipes with exclusions

--- lib/fetchRecipes.ts
+++ lib/fetchRecipes.ts
@@ -1,3 +1,15 @@
+type RecipeType = {
+  id: string;
+  name: string;
+  allIngredientsWithUsages: {
+    ingredient: {
+      name: string;
+      categoryValues: { name: string; category: { name: string } }[];
+    };
+  }[];
+  categoryValues: { name: string; category: { name: string } }[];
+};
+
 function fetchRecipes() {
   const query = `
   query RecipesQuery {
@@ -5,10 +17,73 @@ function fetchRecipes() {
       recipes {
         id
         name
+        categoryValues {
+          name
+          category {
+            name
+          }
+        }
+        allIngredientsWithUsages {
+          ingredient {
+            id
+            name
+            categoryValues {
+              name
+              category {
+                name
+              }
+            }
+          }
+        }
       }
     }
   }
   `;
-  const data = executeGalleyRequest({ query }).data.viewer.recipes;
-  writeToSheet({ data, sheetName: "recipes-export", headers: ["id", "name"] });
+  const data = executeGalleyRequest({ query }).data.viewer
+    .recipes as RecipeType[];
+  writeToSheet({
+    data,
+    sheetName: "recipes-export",
+    headers: ["id", "name", "exclusions", "plan type"],
+    rowFormatter: row => [
+      row.id,
+      row.name,
+      buildExclusionsList(row),
+      getPlanType(row)
+    ]
+  });
 }
+
+const buildExclusionsList = (row: RecipeType): string => {
+  const exclusionNamesToIngredients = row.allIngredientsWithUsages.reduce(
+    (memo, { ingredient }) => {
+      const exclusionNames = ingredient.categoryValues
+        .filter(({ category }) => category.name === "exclusion")
+        .map(({ name }) => name);
+      exclusionNames.map(exclusionName => {
+        if (!memo[exclusionName]) {
+          memo[exclusionName] = [];
+        }
+        memo[exclusionName].push(ingredient.name);
+      });
+
+      return memo;
+    },
+    {}
+  );
+  return Object.keys(exclusionNamesToIngredients)
+    .map(
+      exclusionName =>
+        `${exclusionName} [${exclusionNamesToIngredients[exclusionName].join(
+          ", "
+        )}]`
+    )
+    .join("\n");
+};
+
+const getPlanType = (row: RecipeType) => {
+  return row.categoryValues
+    .filter(({ category }) => category.name === "meal plan")
+    .map(({ name }) => name)
+    .join(", ");
+};
--- lib/writeToSheet.ts
+++ lib/writeToSheet.ts
@@ -1,17 +1,20 @@
-function writeToSheet({
+function writeToSheet<TData extends object>({
   data,
   sheetName,
-  headers
+  headers,
+  rowFormatter
 }: {
-  data: { id: string; name: string }[];
+  data: TData[];
   sheetName: string;
   headers: string[];
+  rowFormatter?: (row: TData) => any[];
 }) {
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   const outputSheet = spreadsheet.getSheetByName(sheetName);
   if (!outputSheet) {
     throw new Error("Couldn't find sheet " + sheetName);
   }
-  const range = outputSheet.getRange(1, 1, data.length + 1, 2);
-  range.setValues([headers, ...data.map(({ id, name }) => [id, name])]);
+  const range = outputSheet.getRange(1, 1, data.length + 1, headers.length);
+  rowFormatter = rowFormatter || (row => headers.map(attr => row[attr]));
+  range.setValues([headers, ...data.map(rowFormatter)]);
 }
